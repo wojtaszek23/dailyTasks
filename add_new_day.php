14af7c141c4baed152a7331e7411697eab9f4842
wojtaszek23 <wojtaszek23@wp.pl>
2021-10-01 11:34:11 +0200

    refactored add_new_day, which will be executed as cron task in server everyday after midnight

--- add_new_day.php
+++ add_new_day.php
@@ -1,17 +1,9 @@
 <?php
 
-session_start();
-
-if(!empty($_POST))
-{
   require_once "../connection_strings.php";
   
   $connection = new mysqli($host, $db_user, $password, $db_name_dailyTasks);
  
-  $challange_name = $_SESSION['nick']."_challange";
-
-  $challange_name_results = $challange_name."_results";
-
   if($connection->connect_errno != 0)
   {
     throw new Exception(myslqi_connect_errno());
@@ -22,10 +14,9 @@ if(!empty($_POST))
 
 foreach($table as $tables)
 {
-  $connection->query("INSERT INTO `$challange_name_results`() VALUES();");
+  $connection->query("INSERT INTO `$table`() VALUES();");
 }
 
   $connection->close();
-}
 
 ?>
